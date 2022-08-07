21bcc032d648868fa2197ac99077533feee72ef8
kirill <silantiev82@gmail.com>
2022-08-07 20:55:51 +0300

    if - else цикл с применением команды ToLower()

---
+++ Example005_if-else/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите имя пользователя");
+string username = Console.ReadLine();
+if(username.ToLower() == "маша")
+{
+    Console.WriteLine("Ура,Это же маша!");
+}
+else
+{
+    Console.Write("Привет,");
+    Console.WriteLine(username);
+}
