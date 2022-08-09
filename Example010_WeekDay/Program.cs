f9d270f90315fc40d67e0d7255c3edb9703c55c9
kirill <silantiev82@gmail.com>
2022-08-09 23:22:58 +0300

    WeekDay.  Пользователь вводит ыифру, а программа выводит соответствующий цыфре день

---
+++ Example010_WeekDay/Program.cs
@@ -0,0 +1,30 @@
+﻿Console.WriteLine("Введите число от 1 до 7");
+int dayNum = Convert.ToInt32(Console.ReadLine());
+if(dayNum == 1)
+{
+Console.WriteLine("Понедельник");
+}
+if(dayNum == 2)
+{
+Console.WriteLine("Вторник");
+}
+if(dayNum == 3)
+{
+Console.WriteLine("Среда");
+}
+if(dayNum == 4)
+{
+Console.WriteLine("Четверг");
+}
+if(dayNum == 5)
+{
+Console.WriteLine("Пятница");
+}
+if(dayNum == 6)
+{
+Console.WriteLine("Суббота");
+}
+if(dayNum == 7)
+{
+Console.WriteLine("Воскресенье");
+}
