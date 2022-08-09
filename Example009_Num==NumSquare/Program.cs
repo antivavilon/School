24c5014f5d9b263b610c834e3c62ca4baf777a7b
kirill <silantiev82@gmail.com>
2022-08-09 21:21:55 +0300

    Является ли второе число квадратом первого

---
+++ Example009_Num==NumSquare/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите первое число");
+int numA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите ввторое число");
+int numB = Convert.ToInt32(Console.ReadLine());
+if(numA / numB == numB )
+   {
+    Console.WriteLine(numA  + "является квадратом" + numB);
+   }
+   else
+   {   
+    Console.WriteLine(numA + " не является квадратом" + numB);    
+   }
