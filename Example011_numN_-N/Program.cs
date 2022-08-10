df4a27f244bec755b76193910a22d4ec5eacc562
kirill <silantiev82@gmail.com>
2022-08-10 13:44:07 +0300

    -N...N Пользователь вводит положительное число. Программа должна вывести все числа от отредцательного значения до положительного. Например 5. Нужно вывести от -5 до 5

---
+++ Example011_numN_-N/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите число");
+int num = Convert.ToInt32(Console.ReadLine());
+    
+int negativeNum = num * (-1);
+   while(negativeNum <= num)
+      {
+       Console.WriteLine(negativeNum);
+       negativeNum++;
+       }
+       
+       
