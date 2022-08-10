1a67dae2330583662d47de5f34bd733f0dc4fe96
kirill <silantiev82@gmail.com>
2022-08-10 16:18:56 +0300

    MaximalFromThree. Пользователь вводит три числа, а программа выдает в консоль максимальное из них.

---
+++ Example013_MaximalFromThree/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Введите первое число: ");
+int firstNum = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int secondNum = Convert.ToInt32(Console.ReadLine());
+Console.Write
+("Введите третье  число: ");
+int thirdNum = Convert.ToInt32(Console.ReadLine());
+int max = firstNum;
+if(firstNum > max) max = firstNum;
+if(secondNum > max) max = secondNum;
+if(thirdNum > max) max = thirdNum;
+Console.Write("Максимальное число: ");
+Console.WriteLine(max);
