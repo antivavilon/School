2f94835341c3e3485e475f8837d32a42afb75f24
kirill <silantiev82@gmail.com>
2022-08-10 14:49:17 +0300

    MaxNum. Пользователь вводит два числа, а программа сравнивает и выдает в консоль маскимальное из них.

---
+++ Example012_Max_Num/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Введите первое число");
+int numA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите первое число");
+int numB = Convert.ToInt32(Console.ReadLine());
+int max = numA;
+if(numA > max) max = numA;
+if(numB > max) max = numB;
+Console.Write("Максимальное число:");
+Console.WriteLine(max);
