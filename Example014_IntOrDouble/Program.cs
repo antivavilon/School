fecc87604ba1a702ae68ffb4bcd32961d12a32e3
kirill <silantiev82@gmail.com>
2022-08-10 18:00:33 +0300

    MaxNum. Пользователь вводит два числа, а программа сравнивает и выдает в консоль маскимальное из них.

---
+++ Example014_IntOrDouble/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+if (num % 2 == 0)
+{
+   Console.Write(num + " это четное число"); 
+}
+else
+{
+    Console.Write(num + " это нечетное число"); 
+}
