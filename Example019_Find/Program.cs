f3ec4d03193961d84f281645447d23e5255ecf69
kirill <silantiev82@gmail.com>
2022-08-13 15:27:38 +0300

    FindNum. Пользователь вводит число из массива,а программа находит его методом сравнения и выводит на экран.

---
+++ Example019_Find/Program.cs
@@ -0,0 +1,12 @@
+﻿int[ ]array = {3, 12, 8, 19, 333, 1, 91,};
+int n = array.Length;
+int find = 333;
+int index = 0;
+while (index < n)
+{
+ if(array[index]==find)
+ {
+    Console.WriteLine (array[index]);
+ }
+ index++;
+}
