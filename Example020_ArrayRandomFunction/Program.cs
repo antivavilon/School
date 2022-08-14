9985f7364212d074750a3f2905b47919fb147f61
kirill <silantiev82@gmail.com>
2022-08-14 20:42:30 +0300

    Внесли изменения

--- Example020_ArrayRandomFunction/Program.cs
+++ Example020_ArrayRandomFunction/Program.cs
@@ -1,20 +1,52 @@
-﻿ void FillArray(int[]collection)
-{
-    int length = collection.Length;
-    int index = 0;
-    while (index < length)
-    {
-        collection[index] = new Random().Next(0,10);
-        index++; 
-    }
+﻿
+            void FillArray(int[]collection)                                  
+           {                                                                 
+               int length = collection.Length;                              
+               int index = 0;                                        
+               while (index < length)                                        
+               {
+                   collection[index] = new Random().Next(0,10);              
+                   index++;                                                  
+               }
 
-}
+           }
 
- сдуфк
- 
-array[7] = 4;
-PrintArray(array);
-Console.WriteLine();
-int pos = IndexOf(array, 12);
-Console.WriteLine(pos);
+            void PrintArray(int[]col)                                         
+           {
+               int count = col.Length;                                       
+               int position = 0;                                              
+              while (position < count)                                        
+               {
+                  Console.WriteLine(col[position]);                          
+                   position++;                                                
+               }
 
+           }
+
+                                                               
+                                                                        
+           int IndexOf(int[] collection, int find)     
+           {                  
+           int count = collection.Length;                                       
+           int index = 0;                                           
+           int position = -1;                                               
+            
+           while(index < count)                                                                                          
+            {
+               if(collection[index] == find)                                    
+               {
+                position = index;                                               
+                break;
+     
+               }
+               index = index + 1;                                               
+            }
+           return position;                                                     
+           }
+                                                                                
+           int[] array = new int[10];                       
+           FillArray(array);                                                   
+           PrintArray(array);                                                
+           Console.WriteLine();                                               
+           int pos = IndexOf(array, 3);                                       
+           Console.WriteLine(pos);                                            
