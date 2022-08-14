4ffcfbbfe7ba01d48e97be6d0e3544eda8659d8f
kirill <silantiev82@gmail.com>
2022-08-14 17:28:23 +0300

    ArrayRandomFunctio. Наполнили массив с помощью функции, вывели все значения с помощью функции и нашои загаданное значение с помощью функции

---
+++ Example020_ArrayRandomFunction/Program.cs
@@ -0,0 +1,20 @@
+﻿ void FillArray(int[]collection)
+{
+    int length = collection.Length;
+    int index = 0;
+    while (index < length)
+    {
+        collection[index] = new Random().Next(0,10);
+        index++; 
+    }
+
+}
+
+ сдуфк
+ 
+array[7] = 4;
+PrintArray(array);
+Console.WriteLine();
+int pos = IndexOf(array, 12);
+Console.WriteLine(pos);
+
