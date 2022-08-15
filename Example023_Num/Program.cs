c5bcdd22014169ceea82ef6738425541f7f6bd6f
kirill <silantiev82@gmail.com>
2022-08-15 19:47:38 +0300

    На входе принимаем два числа и проверяем кратно ли второе число первому. Если нет, то нужно вывенсти остаток отделения

---
+++ Example023_Num/Program.cs
@@ -0,0 +1,11 @@
+﻿int firstDigit = 130;
+int secondDigit = 5;
+double result = firstDigit % secondDigit;
+if (result != 0) 
+{
+Console.WriteLine($"Первое число не кратно второму. Остато от деления: {result}");
+}
+else
+{
+    Console.WriteLine("Первое число  кратно второму.");
+}
