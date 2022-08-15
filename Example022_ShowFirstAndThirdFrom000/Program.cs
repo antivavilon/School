93c3b98c4194d823c86afc15069de29212a644ed
kirill <silantiev82@gmail.com>
2022-08-15 21:32:20 +0300

    генерируем случайное трехзначнеое число и делаем из него двухзначное, убрав из середины цыфру

--- Example022_ShowFirstAndThirdFrom000/Program.cs
+++ Example022_ShowFirstAndThirdFrom000/Program.cs
@@ -1,5 +1,4 @@
 ﻿int num = new Random().Next(100, 999);
 Console.WriteLine(num);
-int firstDigit = num / 100;
-int thirdDigit = num % 10;
-Console.WriteLine($"Первая цифра числа {num} это {firstDigit}, а третье {thirdDigit} ");
+int result = (num/100)*10 + num % 10;
+Console.WriteLine(result);
