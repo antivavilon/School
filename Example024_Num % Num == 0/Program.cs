5b1b8529a4236ce9cf0ed41cec22fe2263a9e47e
kirill <silantiev82@gmail.com>
2022-08-15 23:13:32 +0300

    Вводится одно число. На выходе нужно проверить кратно ли оно одновременно двум числам. Используем оператор логического &&

---
+++ Example024_Num % Num == 0/Program.cs
@@ -0,0 +1,23 @@
+﻿//int num = 161;                                                
+//if (num % 7 == 0)
+//{
+ //if (num % 23 == 0 ) 
+ // {
+ //Console.WriteLine("Число кратно одновременно  7 и 23");
+ // }
+ // else
+ // {
+ //   Console.WriteLine("Число не кратно одновременно  7 и 23");
+ // } 
+//}
+
+
+int num = 99;                                                
+if (num % 7 == 0 && num % 23 == 0 ) 
+{
+ Console.WriteLine("Число кратно одновременно  7 и 23");
+  }
+  else
+  {
+    Console.WriteLine("Число не кратно 7 и 23");
+  } 
