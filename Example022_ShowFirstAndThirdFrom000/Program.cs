1b7d715c7e95cd58548057619bdfc9508fa8a98b
kirill <silantiev82@gmail.com>
2022-08-15 18:26:34 +0300

    На входе генерируем трехзначное случайное число, а на выходе нужно вывести первую и третью цыфру числа.

---
+++ Example022_ShowFirstAndThirdFrom000/Program.cs
@@ -0,0 +1,5 @@
+﻿int num = new Random().Next(100, 999);
+Console.WriteLine(num);
+int firstDigit = num / 100;
+int thirdDigit = num % 10;
+Console.WriteLine($"Первая цифра числа {num} это {firstDigit}, а третье {thirdDigit} ");
