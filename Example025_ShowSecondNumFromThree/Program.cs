3abbb702161fb600d7fdd9c55850dcdaca884264
kirill <silantiev82@gmail.com>
2022-08-16 17:59:04 +0300

    ShowSecondNunFromThree_Lesson2_HomeWork. Дается трехзначное число. На выходе нужно показать вторую цифру этого числа.

--- Example025_ShowSecondNumFromThree/Program.cs
+++ Example025_ShowSecondNumFromThree/Program.cs
@@ -1,4 +1,6 @@
-﻿int num = new Random().Next(100, 1000);
-Console.WriteLine (num);
+﻿Console.WriteLine("Введите число от 100 до 1000");
+int num = Convert.ToInt32(Console.ReadLine());
+//int num = new Random().Next(100, 1000);
+//Console.WriteLine (num);
 int result = (num / 10 )% 10;
 Console.WriteLine($"Вторая цифра числа {num} это: {result}");
