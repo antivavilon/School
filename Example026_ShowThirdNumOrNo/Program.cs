e1cf91081e75540a0e5aaf4a7b12a12ed35b0496
kirill <silantiev82@gmail.com>
2022-08-16 16:04:25 +0300

    Вводится одно число. На выходе программа печатает число и третью цыфру этого числа или сообщает, что у числа нет третьей цыфры

---
+++ Example026_ShowThirdNumOrNo/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Ведите число");
+int num = Convert.ToInt32(Console.ReadLine());
+//int num = new Random().Next(10, 1000);
+Console.WriteLine(num);
+ 
+string numString = num.ToString();
+if (num / 100 == 0 )
+{
+  Console.WriteLine($"У числа {num} нет третьей цифры ");  
+}
+else
+{
+
+Console.WriteLine ($"У числа {num} третья цыфра это {numString[2]}");
+
+
+}
