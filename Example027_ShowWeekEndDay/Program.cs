cf8ca36dafa1c5a809c1e21f43c4365e1af9bcb3
kirill <silantiev82@gmail.com>
2022-08-16 17:43:35 +0300

    ShowWeekEndDay. Вводится число от  1 до 7. На выходе программа говорит рабочий это  день или будний

---
+++ Example027_ShowWeekEndDay/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите число от 1 до 7");
+int num = Convert.ToInt32(Console.ReadLine());
+if (num == 1 || num == 2 || num == 3 || num == 4 || num == 5 )
+{
+    Console.WriteLine("Это будний день.");
+}
+if (num == 6 || num ==7 )
+{
+    Console.WriteLine("Это выходной день!");     
+}
