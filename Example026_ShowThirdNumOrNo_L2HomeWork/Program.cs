19cce099d4d4d97bd47707610aeb0204c655f917
kirill <silantiev82@gmail.com>
2022-08-17 15:46:37 +0300

    ShowThirdOrNo_Lesson2HomeWork. Вводится одно число. На выходе программа печатает число и третью цыфру этэтого числа или сообщает, что у числа нет третьей цыфры

---
+++ Example026_ShowThirdNumOrNo_L2HomeWork/Program.cs
@@ -0,0 +1,33 @@
+﻿Console.WriteLine("Ведите число");
+string num = Console.ReadLine();
+int size = num.Length; 
+if (size < 3 )
+{
+   Console.WriteLine($"У числа нет третьей цифры ");  
+}
+else
+{
+   Console.WriteLine(num [2] );
+}
+
+
+
+
+
+
+
+
+//Console.WriteLine("Ведите число");
+//int num = Convert.ToInt32(Console.ReadLine());
+     //int num = new Random().Next(10, 1000);
+//Console.WriteLine(num);
+ 
+//string numString = num.ToString();
+//if (num / 100 == 0 )
+//{
+  // Console.WriteLine($"У числа {num} нет третьей цифры ");  
+//}
+//else
+//{
+//   Console.WriteLine ($"У числа {num} третья цыфра это {numString[2]}");
+//
