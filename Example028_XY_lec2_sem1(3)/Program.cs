6152046923d70b7562a69f71d0a943ad748aa636
kirill <silantiev82@gmail.com>
2022-08-18 00:23:42 +0300

    Напишите программу, которая принимает на вход координаты дыух  и нгаходит расстояние между ними в 2D пространстве.A(3,6), B(2,1).

---
+++ Example028_XY_lec2_sem1(3)/Program.cs
@@ -0,0 +1,15 @@
+﻿// 17. Напишите программу, 
+// которая на вход принимает координаты точки
+//(X и Y),причем x не = 0, y не = 0 и выдает номер четверти плоскости,
+// в которй находится эта точка
+
+
+
+Console.WriteLine("Введите координату по оси x:");
+int x = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите координату по оси y:");
+int y = Convert.ToInt32(Console.ReadLine());
+if(x > 0 && y > 0 ) Console.WriteLine("Первая четверть");
+if(x < 0 && y > 0 ) Console.WriteLine("Вторая четверть");
+if(x < 0 && y < 0 ) Console.WriteLine("Третья четверть");
+if(x > 0 && y < 0 ) Console.WriteLine("Четвертая четверть");
---
+++ Example030_WhatSbetweenAandB_lec2_sem2(3)/Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите программу, которая принимает на вход координаты дыух 
+// и нгаходит расстояние между ними в 2D пространстве.A(3,6), B(2,1)
+int xa = 7;
+int ya = -5;
+int xb = 1;
+int yb = -1;
+int xc = xa - xb;
+int yc = ya - yb ;
+//int inter = ((xa - xb)*(xa - xb)) + ( (ya - yb)*(ya - yb) )
+int inter = ((7 - 1)*(7 - 1)) + ((-5 - (-1))*(-5 - (-1)));
+//int inter = ((3 - 2)*(3 - 2)) + ((6 - 1)*(6 - 1));
+double result =Math.Sqrt(inter);
+Console.WriteLine($"Длинна отрезка: {result}");
