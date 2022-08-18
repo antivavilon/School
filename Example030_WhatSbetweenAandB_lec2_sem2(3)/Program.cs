742be786ecd2dc19740968bda26ad4726f13772b
kirill <silantiev82@gmail.com>
2022-08-18 14:29:49 +0300

    Задача21. Напишите программу, которая принимает на вход координаты дыух чисел  и находит расстояние между ними в 2D пространстве.

--- Example030_WhatSbetweenAandB_lec2_sem2(3)/Program.cs
+++ Example030_WhatSbetweenAandB_lec2_sem2(3)/Program.cs
@@ -1,13 +1,21 @@
-﻿// Напишите программу, которая принимает на вход координаты дыух 
-// и нгаходит расстояние между ними в 2D пространстве.A(3,6), B(2,1)
-int xa = 7;
-int ya = -5;
-int xb = 1;
-int yb = -1;
-int xc = xa - xb;
-int yc = ya - yb ;
-//int inter = ((xa - xb)*(xa - xb)) + ( (ya - yb)*(ya - yb) )
-int inter = ((7 - 1)*(7 - 1)) + ((-5 - (-1))*(-5 - (-1)));
+﻿//Задача21. Напишите программу, которая принимает на вход координаты дыух 
+// и находит расстояние между ними в 2D пространстве.
+//(A(3,6), B(2,1) result = 5,09
+ // A(7,-5), B(1,-1) result = 2,21
+//Для решения задачи используется формула(Теорема Пифагора)
+//d^2= (х2— х1)^2+ (y2— y1)^2
+//Использовали метод, который выводит корень: Math.Sqrt(num)
+// Использовали метод, который возводит число или выражение в степень: Math.Sqrt(num,2)
+// Использовали метод, который округляет до нужного нам порядка после запятой: Math.Round(num,2)
+Console.WriteLine("Введите координату x точки A" );
+int xa = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите координату y точки A" );
+int ya = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите координату x точки B" );
+int xb = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите координату y точки B" );
+int yb = Convert.ToInt32(Console.ReadLine());
+//int inter = ((7 - 1)*(7 - 1)) + ((-5 - (-1))*(-5 - (-1)));
 //int inter = ((3 - 2)*(3 - 2)) + ((6 - 1)*(6 - 1));
-double result =Math.Sqrt(inter);
-Console.WriteLine($"Длинна отрезка: {result}");
+double result = Math.Sqrt((Math.Pow(xa - xb,2)) + (Math.Pow(ya - yb,2)));;
+Console.WriteLine($"Длинна отрезка:{Math.Round(result,3)}");
