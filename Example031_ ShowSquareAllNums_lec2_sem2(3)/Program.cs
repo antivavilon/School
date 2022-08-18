864d817abd533785d3c2e068e26d4dd1ecedb832
kirill <silantiev82@gmail.com>
2022-08-18 17:19:44 +0300

    Задачяа 2. Напишите программу, которая принимает на вход число N и выдает таблицу квадратов чисел от 0 до N

---
+++ Example031_ ShowSquareAllNums_lec2_sem2(3)/Program.cs
@@ -0,0 +1,21 @@
+﻿//Задачяа 22.Напишите программу, которая принимает на вход число N и выдает таблицу квадратов чисел от 0 до N
+
+//Решение с помощью цикла while
+//Console.WriteLine("Ведите число от 1 до 10");
+//int num =Convert.ToInt32(Console.ReadLine());
+//double digit = 1;
+//while (digit < num)
+//{
+    //Console.WriteLine(Math.Pow(digit,2));
+  //  digit++;
+//}
+
+//Решение с помощью цикла for
+Console.WriteLine("Ведите число от 1 до 10");
+int N =Convert.ToInt32(Console.ReadLine());
+for (int start=1; start <= N; start++) 
+{
+    Console.WriteLine(Math.Pow(start,2));
+}
+
+
