f86be00011a05252be2c5c21f37bf2740c687cbb
kirill <silantiev82@gmail.com>
2022-08-20 15:43:01 +0300

    //Задача 23. Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.

---
+++ Example034_ShowAllnumCubsFrom1toN_sem2(3)_homeWork/Program.cs
@@ -0,0 +1,11 @@
+﻿//Задача 23. Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+Console.WriteLine("Введите число");
+int N = Convert.ToInt32(Console.ReadLine());
+double digitHelp = 1;
+ while(digitHelp <= N)
+
+{
+    double digitCube = Math.Pow(digitHelp,3);
+    digitHelp ++;
+    Console.WriteLine(digitCube);
+}
