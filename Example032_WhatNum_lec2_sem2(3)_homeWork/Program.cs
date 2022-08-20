6b457c2e96a421f10449b44804462c96d5b5aaee
kirill <silantiev82@gmail.com>
2022-08-20 13:21:11 +0300

    Задача 19.Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.

---
+++ Example032_WhatNum_lec2_sem2(3)_homeWork/Program.cs
@@ -0,0 +1,152 @@
+﻿//Задача 19.Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом
+
+
+Console.WriteLine("Введите пятизначное число");
+string num =(Console.ReadLine());
+Console.WriteLine(num);
+if (num[0] == num[4] && num[1] == num[3] )
+{
+ Console.WriteLine ("Это число палиндром");  
+}
+else
+{
+    Console.WriteLine("Это число не палиндром");
+}
+//int helpNum = num;
+//int count = 0;
+//while (helpNum > 0)
+ //{
+  //helpNum = helpNum / 10;
+  //count++;
+ //}
+
+
+
+
+
+
+
+
+
+
+
+
+
+//Console.WriteLine("Введите число");
+//int num = Convert.ToInt32(Console.ReadLine());
+//int helpNum = num;
+//int count = 0;
+//while (helpNum > 0)
+ //{
+  //helpNum = helpNum / 10;
+  //count++;
+ //}
+   
+//string numString = Convert.ToString(num);
+
+//if (count == 1)Console.WriteLine("Это чиссло не может быть палиндром");
+//if (count == 2 )
+//{
+    //if (numString[0] == numString[1])
+    //{
+      //  Console.WriteLine("Это чиссло палиндром ");
+    //}
+    //else
+    //{
+    //    Console.WriteLine("Это чиссло не палиндром");
+  //  }
+//}
+
+//if (count == 3  )
+//{
+    //if (numString[0] == numString[2])
+    //{
+     //   Console.WriteLine("Это чиссло палиндром ");
+   // }
+    //else
+    //{
+    //    Console.WriteLine("Это чиссло не палиндром");
+  //  }
+//}
+
+//if (count == 4 )
+//{
+    //if (numString[0] == numString[3] && numString[1] == numString[2] ) 
+    //{
+    //    Console.WriteLine("Это чиссло палиндром ");
+    //}
+    //else
+    //{
+    //    Console.WriteLine("Это чиссло не палиндром");
+  //  }
+//}
+
+//if (count == 5 )
+//{
+    //if (numString[0] == numString[4] && numString[1] == numString[3] ) 
+    //{
+      //  Console.WriteLine("Это чиссло палиндром ");
+    //}
+    //else
+    //{
+    //    Console.WriteLine("Это чиссло не палиндром");
+  //  }
+//}
+
+//if (count == 6  )
+//{
+    //if (numString[0] == numString[5] && numString[1] == numString[4] && numString[2] == numString[3]  ) 
+    //{
+     //   Console.WriteLine("Это чиссло палиндром ");
+    //}
+    //else
+    //{
+    //    Console.WriteLine("Это чиссло не палиндром");
+  //  }
+//}
+
+//if (count == 7  )
+//{
+    //if (numString[0] == numString[6] && numString[1] == numString[5] && numString[2] == numString[4]  ) 
+    //{
+    //    Console.WriteLine("Это чиссло палиндром ");
+    //}
+    //else
+    //{
+    //    Console.WriteLine("Это чиссло не палиндром");
+  //  }
+//}
+ 
+ //if (count == 8  )
+//{
+    //if (numString[0] == numString[7] && numString[1] == numString[6] && numString[2] == numString[5] && numString[3] == numString[4]    ) 
+    //{
+      //  Console.WriteLine("Это чиссло палиндром ");
+    //}
+    //else
+    //{
+      //  Console.WriteLine("Это чиссло не палиндром");
+  //  }
+//}
+
+//if (count == 9   )
+//{
+    //if (numString[0] == numString[8] && numString[1] == numString[7] && numString[2] == numString[6] && numString[3] == numString[5]   ) 
+    //{
+      //  Console.WriteLine("Это чиссло палиндром ");
+    //}
+    //else
+    //{
+      //  Console.WriteLine("Это чиссло не палиндром");
+    //}
+//}
+                  
+            
+
+          
+             
+
+
+
+
+   
