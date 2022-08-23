141b711610625f07a7212a406a34731255f86554
kirill <silantiev82@gmail.com>
2022-08-24 00:06:14 +0300

    адача 25. Напишите цикл, который  на вход принимает два числа (А и B)  и возводит A в натуральную степень B.

---
+++ Example042 zadacha25_sem1(4)_homeWork/Program.cs
@@ -0,0 +1,28 @@
+﻿//Задача 25. Напишите цикл, который  на вход принимает два числа (А и B)  и возводит A в натуральную степень B.
+
+//Console.WriteLine("Введите числа А");
+// int A = Convert.ToInt32(Console.ReadLine());
+//Console.WriteLine("Введите числа B");
+//int B = Convert.ToInt32(Console.ReadLine());
+//int  result = A;
+//for (int i = 0; i < B-1; i++)
+//{
+ //     result = result * A ;
+//}
+//Console.WriteLine(result);
+
+//Решение задачи с помощбю метода.
+Console.WriteLine("Введите числа А");
+ int A = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите числа B");
+int B = Convert.ToInt32(Console.ReadLine());
+int Method(int firstDigit, int secondDigit)
+{
+int result = firstDigit;
+for (int i = 0; i < secondDigit-1; i++)
+{
+    result = result * firstDigit;
+}
+ return result;
+}
+ Console.WriteLine($"числож: {Method( A, B)}");
