b579b007f9e9b05991a53026601af03601e9af55
kirill <silantiev82@gmail.com>
2022-08-24 13:35:52 +0300

    Задача 27. Напишите программу, которая принимает на вход число и выдает сумму цыфр в числе

---
+++ Example043 zadacha27_sem1(4)_homeWork/Program.cs
@@ -0,0 +1,37 @@
+﻿//Задача 27. Напишите программу, которая принимает на вход число и выдаетсумму цыфр в числе
+
+//1 Решение через цикл.
+//Console.WriteLine("Введите Число");
+//int digit = Convert.ToInt32(Console.ReadLine());
+//int sum =0;
+//while (digit > 0)
+//{                               
+ //   int a = digit % 10;
+//     sum = sum + a;
+//    digit = digit /10;
+    
+//}
+//Console.WriteLine(sum);
+
+
+
+//2 Решение через цикл в  методе.
+
+Console.WriteLine("Введите Число");
+int digit = Convert.ToInt32(Console.ReadLine());
+
+int SumNumDigit(int num)
+{
+int result = 0;
+while (num > 0)
+{
+ int a = num % 10;
+  num = num / 10;
+  result = result + a;
+}
+return result;
+}
+
+Console.WriteLine($"сумма цифр этого числа {SumNumDigit(digit)}");
+//int test = SumNumDigit(digit); // Альтернативный метод вывода
+//Console.WriteLine(test);   // Альтернативный метод вывода
