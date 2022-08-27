8fd6d7cf083340c216bd53bb0479b7797c01de2d
kirill <silantiev82@gmail.com>
2022-08-27 22:05:50 +0300

    Задача 32. Напишите программу, замена элементов массива: положительные элементы замените на соответствую  отредцательные эоементы и наоборот. [-4, -8, 8, 2] -> [4, 8, -8, -2]

---
+++ Example047 zadacha32_lec4_sem1(5)/Program.cs
@@ -0,0 +1,23 @@
+﻿//Задача 32. Напишите программу, замена элементов массива: положительные элементы замените на соответствующие
+  //         отредцательные эоементы и наоборот. [-4, -8, 8, 2] -> [4, 8, -8, -2]int
+  Console.WriteLine("Введите числа в массив");
+  
+int[] nums = new int[5];
+for (int i = 0; i < nums.Length; i++)
+   {
+   nums[i] = Convert.ToInt32(Console.ReadLine());    
+   }
+   Console.Write($"массив: [{string.Join (",",nums)}]");
+   Console.WriteLine();
+
+   int[]ChangeTo( int[]arr) 
+    {
+     for (int i = 0; i < arr.Length; i++)
+      {
+       arr[i] = arr[i] *(-1);  
+      }
+     return arr;
+    }
+
+int[] test = ChangeTo(nums);
+  Console.Write($"массив: [{string.Join (",",nums)}]");
