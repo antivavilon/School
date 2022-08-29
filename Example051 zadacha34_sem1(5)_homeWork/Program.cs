ac9a0f210852931692755b49f7121ccf5346ac77
kirill <silantiev82@gmail.com>
2022-08-29 21:11:59 +0300

    Задача 34. Задайте массив, заполненный случайными положительными числами. Напишите программу, которая покажет количество четных чисел в массиве.

---
+++ Example051 zadacha34_sem1(5)_homeWork/Program.cs
@@ -0,0 +1,33 @@
+﻿//Задача 34. Задайте массив, заполненный случайными положительными числами.
+  //          Напишите программу, которая покажет количество четных чисел в массиве.
+
+  //Первый вариант решения.
+  int[]arr = new int[12];
+  for (int i = 0; i <arr.Length; i++)
+  {
+   arr[i] = new Random().Next(999);
+  }
+   Console.WriteLine($"массив:[{string.Join (",",arr)}]");
+
+
+  int count = 0;
+  for (int i = 0; i < arr.Length; i++)
+  if (arr[i] % 2 == 0) count++;
+  if(count == 0) Console.WriteLine($"В этом массиве {count} четных чисел ");
+  if(count == 1) Console.WriteLine($"В этом массиве {count} четнoe число ");
+  if(count <= 4 && count > 1 ) Console.WriteLine($"В этом массиве {count} четных числа ");
+  if(count > 4) Console.WriteLine($"В этом массиве {count} четных чисел ");
+  //Второй вариант решения(метод).
+
+//  int getCount(int[]array)
+//  {
+//    int count =0;
+//    for (int i = 0; i < array.Length; i++)
+//    {
+//    if (arr[i] % 2 == 0) count++;
+//    }
+//    return count;
+//  }
+//
+//  int resultCount = getCount(arr);
+//  Console.WriteLine($"В этом массиве {resultCount} четных чисел ");
