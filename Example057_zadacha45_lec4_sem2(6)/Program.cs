23054867e694256fc4c92f5bcc38ae62a4157730
kirill <silantiev82@gmail.com>
2022-08-31 16:58:21 +0300

    Задача 45. Напишите программу, которая будет создавать копию заданного массива с помощью поэлементного копирования.

---
+++ Example057_zadacha45_lec4_sem2(6)/Program.cs
@@ -0,0 +1,27 @@
+﻿//Задача 45. Напишите программу, которая будет создавать копию заданного массива с помощью поэлементного копирования.
+          
+
+int size = 12;
+int[]arr = new int [size];
+
+for (int  i = 0;  i < size;  i++)
+{
+    arr[i] = new Random().Next(9);
+}
+Console.WriteLine($"массив:[{string.Join (",",arr)}]");
+
+
+int[] CoppyArr(int[]array)
+{
+int[]result = new int[array.Length];
+for (int i = 0; i <array.Length; i++)
+{
+    result[i] = array[i];
+}
+
+return result;
+}
+
+
+int[]test = CoppyArr(arr);
+Console.WriteLine($"массив:[{string.Join (",",test)}]");
