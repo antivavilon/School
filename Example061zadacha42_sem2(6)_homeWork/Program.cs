2ca05969d61e19e0515a37524e5507d83b1cc256
kirill <silantiev82@gmail.com>
2022-09-04 14:22:27 +0300

    Задача 44.1 Вывести все числа фибоначи до N, но на этот раз черз массив.

--- Example061zadacha42_sem2(6)_homeWork/Program.cs
+++ Example061zadacha42_sem2(6)_homeWork/Program.cs
@@ -1,43 +1,44 @@
-﻿int[]ToDouble(int digit)                         // Создаем метод. Аргумент число, которе нужно конвертировать. На выходе массив с  двоичным числом
+﻿
+int[] ToDouble(int digit)                         // Создаем метод. Аргумент число, которе нужно конвертировать. На выходе массив с  двоичным числом
 {
- int num = 2;
- int count = 2;                                 // Вычисляем длинну массива
- while (num*2 < digit)
- { 
-    num = num *2;
-    count++;
- }  
- int[]arr = new int[count];                      // Создаем массив, в котором возводим цыфру 2 нужное количество раз.
- int help = 2;
- arr[count - 1] = 1;
- arr[count - 2] = 2;
- for (int i = count-3; i >= 0; i--)               // Наполгяем массив с конца. Последний элемент равен 1, следующий равен 2.
- {   
-     help = help*2;                               // В третьем с конца  возводим число во вторую  степень(2*2) , в след. эл. (4*2                                      
-     arr[i] = help;                               // и т.д.
- }
-     //Console.WriteLine($"массив:[{string.Join (",",arr)}]");
-
-int[]result = new int[count];                                       // Создаем массив длч непосредственно двоичного числа
-     //Console.WriteLine($"массив:[{string.Join (",",result)}]");
-int sum = 0;
-for (int i = 0; i < count; i++)
- {
-    sum = sum +arr[i];
-     //Console.WriteLine(sum);
-    if ( sum <= digit ) result[i] = 1;
-    if(sum > digit)
+    int num = 2;
+    int count = 2;                                 // Вычисляем длинну массива
+    while (num * 2 < digit)
     {
-     result[i] = 0;
-     sum = sum - arr[i];
+        num = num * 2;
+        count++;
     }
+    int[] arr = new int[count];                      // Создаем массив, в котором возводим цыфру 2 нужное количество раз.
+    int help = 2;
+    arr[count - 1] = 1;
+    arr[count - 2] = 2;
+    for (int i = count - 3; i >= 0; i--)               // Наполгяем массив с конца. Последний элемент равен 1, следующий равен 2.
+    {
+        help = help * 2;                               // В третьем с конца  возводим число во вторую  степень(2*2) , в след. эл. (4*2                                      
+        arr[i] = help;                               // и т.д.
+    }
+    //Console.WriteLine($"массив:[{string.Join (",",arr)}]");
 
- }
-return result;
+    int[] result = new int[count];                                       // Создаем массив длч непосредственно двоичного числа
+                                                                         //Console.WriteLine($"массив:[{string.Join (",",result)}]");
+    int sum = 0;
+    for (int i = 0; i < count; i++)
+    {
+        sum = sum + arr[i];
+        //Console.WriteLine(sum);
+        if (sum <= digit) result[i] = 1;
+        if (sum > digit)
+        {
+            result[i] = 0;
+            sum = sum - arr[i];
+        }
+
+    }
+    return result;
 }
 
-int [] test = ToDouble(103);                                            // Создаем массив и тестируем метод.
-Console.WriteLine($"массив:[{string.Join (",",test)}]");                 // Выводим массив с двоичным числом            
+int[] test = ToDouble(103);                                            // Создаем массив и тестируем метод.
+Console.WriteLine($"массив:[{string.Join(",", test)}]");                 // Выводим массив с двоичным числом            
 
 
 
@@ -45,4 +46,4 @@ Console.WriteLine($"массив:[{string.Join (",",test)}]");                 /
 //103 -> :[1,1,0,0,1,1,1]
 //47 -> [1,0,1,1,1,1]
 
-  
+                                                                                                                                                                                                                                                                                                                                                                   
