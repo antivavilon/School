6a895ddb154e9c03fd7d96df1b2fc258a55116f2
kirill <silantiev82@gmail.com>
2022-09-09 15:33:43 +0300

    задача

---
+++ Example063zadacha48_sem1(7)/Program.cs
@@ -0,0 +1,31 @@
+﻿int[,] GetArray(int m, int n)
+{
+    int[,] result = new int[m, n];
+    for (int i = 0; i < result.GetLength(0); i++)
+    {
+        for (int j = 0; j < result.GetLength(1); j++)
+            result[i, j] = i + j;
+    }
+
+    return result;
+
+
+}
+
+
+int[,] test = GetArray(3, 9);
+
+void PrintArray(int[,] arr)
+{
+    for (int k = 0; k < arr.GetLength(0); k++)
+    {
+        for (int l = 0; l < arr.GetLength(1); l++)
+        {
+            Console.Write(arr[k, l] + " ");
+        }
+        Console.WriteLine();
+    }
+
+
+}
+PrintArray(test);
