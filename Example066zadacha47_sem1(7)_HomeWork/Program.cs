890b8862bbde304f5186311b62aa9a7f13192fb1
kirill <silantiev82@gmail.com>
2022-09-09 14:00:17 +0300

    // Задача 47. Задайте двумерный массив размером  m + n, заполненный случайными, вещественными числами.

---
+++ Example066zadacha47_sem1(7)_HomeWork/Program.cs
@@ -0,0 +1,32 @@
+﻿
+// Задача 47. Задайте двумерный массив размером  m + n, заполненный случайными, вещественными числами.
+
+double[,] GetArray(int m, int n)
+{
+
+    double[,] result = new double[m, n];
+    for (int i = 0; i < result.GetLength(0); i++)
+    {
+        for (int j = 0; j < result.GetLength(1); j++)
+        {
+            result[i, j] = new Random().Next(0, 9);
+
+        }
+    }
+    return result;
+}
+
+double[,] test = GetArray(7, 12);
+
+void PrintArr(double[,] arr)
+{
+    for (int k = 0; k < arr.GetLength(0); k++)
+    {
+        for (int l = 0; l < arr.GetLength(1); l++)
+        {
+            Console.Write(arr[k, l] + "   ");
+        }
+        Console.WriteLine();
+    }
+}
+PrintArr(test);
